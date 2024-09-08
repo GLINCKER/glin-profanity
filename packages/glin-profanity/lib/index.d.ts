6ae6ba8d9636ecbb7d6d0edf0d2fc714e754b343
GDS K S <39922405+gagandua078@users.noreply.github.com>
2024-09-08 10:58:34 -0500

    Fix: Unexpected token 'export' #29

--- packages/glin-profanity/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Filter } from './filters/Filter';
-export { useProfanityChecker } from './hooks/useProfanityChecker';
-export type { Language, CheckProfanityResult } from './types/types';
-//# sourceMappingURL=index.d.ts.map
