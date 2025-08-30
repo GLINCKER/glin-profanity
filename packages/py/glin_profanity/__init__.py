1ba71903c5c32c51c1a531cc352c0737f3a8d09f
GDS K S <gagandua078@gmail.com>
2025-08-30 16:05:04 -0500

    chore: automated version bump to v2.3.7 [skip ci]

--- packages/py/glin_profanity/__init__.py
+++ packages/py/glin_profanity/__init__.py
@@ -5,7 +5,7 @@ Provides multi-language support, context-aware filtering, and customizable
 configurations for detecting and filtering profane language in text inputs.
 """
  
-__version__ = "2.3.6" 
+__version__ = "2.3.7" 
 __author__ = "glinr"
 __email__ = "contact@glincker.com"
 
